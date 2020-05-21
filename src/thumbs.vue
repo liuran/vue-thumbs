3e437cdccd1736bdbd85f7766b1ce3b55cc77886
L.R <liuran.bj@gmail.com>
2020-05-21 17:53:25 +0800

    换成viewerjs

--- src/thumbs.vue
+++ src/thumbs.vue
@@ -1,59 +1,131 @@
 <template>
   <div>
-    <div class="my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
-      <template v-for="item in items">
-        <figure
-          itemprop="associatedMedia"
-          itemscope
-          itemtype="http://schema.org/ImageObject">
-          <a :href="item.src" itemprop="contentUrl" :data-size="'' + item.w + 'x' + item.h">
-            <img :src="item.msrc" :alt="item.alt" itemprop="thumbnail"/>
-          </a>
-          <figcaption style="display: none" itemprop="caption description">{{item.title}}</figcaption>
-        </figure>
-      </template>
-    </div>
-    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-      <div class="pswp__bg"></div>
-      <div class="pswp__scroll-wrap">
-        <div class="pswp__container">
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-          <div class="pswp__item"></div>
-        </div>
-        <div class="pswp__ui pswp__ui--hidden">
-
-          <div class="pswp__top-bar">
-            <div class="pswp__counter"></div>
-            <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-            <button class="pswp__button pswp__button--share" title="Share"></button>
-            <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-            <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-            <div class="pswp__preloader">
-              <div class="pswp__preloader__icn">
-                <div class="pswp__preloader__cut">
-                  <div class="pswp__preloader__donut"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-            <div class="pswp__share-tooltip"></div>
-          </div>
-          <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-          </button>
-          <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-          </button>
-          <div class="pswp__caption">
-            <div class="pswp__caption__center"></div>
-          </div>
-        </div>
+    <div class="thumb-slide" :id="eid">
+      <div class="inner-viewbox">
+        <ul class="images">
+        <li v-for="item in c_items" :key="item.id">
+          <img :src="item.msrc" :o-src="item.src" :alt="item.alt" >
+          <i class="iconfont icon-delete-" @click="deleteItem(item.id)" />
+          <i class="iconfont icon-upload" @click="changeItem(item.id)"></i>
+          </li>
+         </ul>
       </div>
+
+<!-- 分页 -->
+      <b-pagination v-show="c_total > 0"
+        v-model="currentPage"
+        pills size="sm"
+        :total-rows="c_total"
+        :per-page="c_perPage"
+        :aria-controls="eid"
+        @change="pageChanged"
+      ></b-pagination>
+
     </div>
   </div>
 </template>
 
 <style>
-  @import "~photoswipe/dist/photoswipe.css";
-  @import "~photoswipe/dist/default-skin/default-skin.css";
+@import "~viewerjs/dist/viewer.min.css";
+@import url(//at.alicdn.com/t/font_1763872_9d828bsv4kb.css);
+</style>
+
+<script>
+let _ = require('lodash')
+
+export default {
+  data() {
+    return {
+      currentPage : 1,
+      v_perPage: 5,
+      pages: []
+    };
+  },
+
+  computed: {
+    c_perPage () {
+      return this.perPage || this.v_perPage
+    },
+
+    c_total () {
+      return this.items.length
+    },
+
+    c_items () {
+      return this.pages[this.currentPage - 1]
+    }
+  },
+
+  methods: {
+    pageChanged (page){
+      this.currentPage = page
+    },
+
+    reCalcPages () {
+      this.pages = _.chunk(this.items, this.c_perPage)
+    },
+
+    deleteItem(it) {
+      this.$emit('deleteItem', it)
+    },
+
+    changeItem(it) {
+      this.$emit('changeItem', it)
+    },
+  },
+
+  mounted() {
+    this.reCalcPages()
+  }
+
+
+};
+</script>
+
+
+<style lang="scss" scoped>
+.thumb-slide {
+  overflow: hidden;
+
+  .inner-viewbox {
+    margin-bottom: 30px;
+   
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0;
+
+      li {
+        width: 100px;
+        height: 100px;
+        margin: 5px;
+        list-style-type: none;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          border-radius: 0;
+        }
+
+        i.iconfont {
+          font-size: 10px;
+          margin:5px 2px;
+          color:#ccc;
+        }
+
+        &:hover {
+          cursor: pointer;
+          i.iconfont {
+            color : blue;
+          }
+          }
+      }
+    }
+
+    .pagination{
+      margin-top: 10px;
+    }
+  }
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,40 @@ var webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+    mode: 'development',
     devtool: 'eval-source-map',
+    entry: {
+    index: [ path.resolve(__dirname, './src/index.js') ]
+    },
 
-    entry: __dirname + "/src/index.js",
     module: {
         rules: [
+            { test: /\.scss$/, loaders: ['style-loader', 'css-loader','sass-loader']},
             // use babel-loader for js files
             { test: /\.js$/, loader: 'babel-loader' },
             // use vue-loader for .vue files
             { test: /\.vue$/, loader: 'vue-loader' },
             // use css-loader for .css files
-            { test: /\.css/, loader: 'css-loader' },
+            { test: /\.css/, loader: ['style-loader', 'css-loader'] },
             // use url-loader for .(png|jpe?g|gif|svg) files
             { test: /\.(png|jpe?g|gif|svg)(\?.*)?$/, loader: 'url-loader' }
         ]
     },
+
     context: __dirname,
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: "vue-thumbs.min.js",
+        // chunkFilename: "[id].chunk.js",
         library: 'VueThumbs',
         libraryTarget: 'umd',
         umdNamedDefine: true,
-
     },
 
     plugins: [
         // make sure to include the plugin for the magic
         new VueLoaderPlugin()
-    ],
-
-    devServer: {
-        contentBase: "./example",//本地服务器所加载的页面所在的目录
-        historyApiFallback: true,//不跳转
-        inline: true//实时刷新
-    }
+    ]
 }
 
 if (process.env.NODE_ENV === 'production') {
